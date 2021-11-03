d93e6f8811740bdd349eb5d04e79f4d12b87862f
Anıl Öztürk <80645722+aniloztrk@users.noreply.github.com>
2021-11-03 16:52:20 +0300

    Update ItemLoreManager.java

--- src/main/java/mixyarmorhealth/Utils/ItemLoreManager.java
+++ src/main/java/mixyarmorhealth/Utils/ItemLoreManager.java
@@ -8,25 +8,31 @@ import org.bukkit.inventory.meta.ItemMeta;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ItemLoreManager {
-    public static void optimizeArmorLore(ItemStack item, Player player){
-        if(!ItemTypeControl.itemIsArmor(item)){
+public class ItemLoreManager 
+{
+    public static void optimizeArmorLore(ItemStack item, Player player)
+    {
+        if(!ItemTypeControl.itemIsArmor(item))
+        {
             player.sendMessage(ChatColor.RED + "Item not found");
             return;
         }
         ItemMeta itemMeta = item.getItemMeta();
 
-        if(PersistentDataContainerUtil.hasInteger(item, "Health")){
+        if(PersistentDataContainerUtil.hasInteger(item, "Health"))
+        {
             int health = PersistentDataContainerUtil.getInteger(item, "Health");
 
-            if(itemMeta.getLore() == null){
+            if(itemMeta.getLore() == null)
+            {
                 ArrayList<String> itemLore = new ArrayList<>();
                 itemLore.add("");
                 itemLore.add(ChatColor.RED + "" + ChatColor.BOLD + "+" + health/2 + "❤");
                 itemLore.add("");
                 itemMeta.setLore(itemLore);
             }
-            else{
+            else
+            {
                 List<String> itemLore = new ArrayList<>(itemMeta.getLore());
                 itemLore.set(1, ChatColor.RED + "" + ChatColor.BOLD + "+" + health/2 + "❤");
                 itemMeta.setLore(itemLore);
