27435fb0f47c4e4a9d0ce8d378f0e6f2ff411050
Anıl Öztürk <80645722+aniloztrk@users.noreply.github.com>
2021-11-03 16:54:07 +0300

    Update RegisterUtil.java

--- src/main/java/mixyarmorhealth/Utils/RegisterUtil.java
+++ src/main/java/mixyarmorhealth/Utils/RegisterUtil.java
@@ -4,11 +4,14 @@ import mixy.armor.health.mixyarmorhealth.MixyArmorHealth;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.event.Listener;
 
-public class RegisterUtil {
-    public static void event(Listener listener){
+public class RegisterUtil 
+{
+    public static void event(Listener listener)
+    {
         MixyArmorHealth.Instance.getServer().getPluginManager().registerEvents(listener, MixyArmorHealth.Instance);
     }
-    public static void command(String commandName, CommandExecutor executor){
+    public static void command(String commandName, CommandExecutor executor)
+    {
         MixyArmorHealth.Instance.getCommand(commandName).setExecutor(executor);
     }
 }
